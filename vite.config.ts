371200016945c808bfe1ddc8409276faad18da9b
StevLynnn <stevelinfriska29@gmail.com>
2025-04-23 18:46:09 +0800

    lanjutt

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,14 @@
 import vue from '@vitejs/plugin-vue';
 import laravel from 'laravel-vite-plugin';
 import path from 'path';
-import tailwindcss from "@tailwindcss/vite";
-import { resolve } from 'node:path';
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/js/app.ts'],
-            ssr: 'resources/js/ssr.ts',
+            ssr: 'resources/js/ssr.ts', // Pastikan file ssr.ts ada, jika tidak bisa dihapus
             refresh: true,
         }),
         tailwindcss(),
@@ -24,8 +23,8 @@ export default defineConfig({
     ],
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './resources/js'),
-            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+            '@': path.resolve(__dirname, 'resources/js'),  // Menyederhanakan path
+            'ziggy-js': path.resolve(__dirname, 'vendor/tightenco/ziggy'),  // Perbaikan alias Ziggy
         },
     },
 });
