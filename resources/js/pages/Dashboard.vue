3195941bb47c86cfc14f1a6b541b8f9ec3948e9d
hafizhahnajwa4 <hafizhahnajwa4@gmail.com>
2025-04-23 19:14:14 +0800

    lanjuttt

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -3,6 +3,8 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
+import MapView from '@/components/MapView.vue';
+
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -16,7 +18,11 @@ const breadcrumbs: BreadcrumbItem[] = [
     <Head title="Dashboard" />
 
     <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="h-[400px] rounded-xl overflow-hidden border border-sidebar-border/70 dark:border-sidebar-border">
+            <MapView />
+            </div>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
+            <!-- 3 Card di atas -->
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
                     <PlaceholderPattern />
@@ -28,9 +34,9 @@ const breadcrumbs: BreadcrumbItem[] = [
                     <PlaceholderPattern />
                 </div>
             </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
-                <PlaceholderPattern />
-            </div>
+
+            <!-- Komponen MapView menggantikan Placeholder bawah -->
+            
         </div>
     </AppLayout>
 </template>
--- resources/js/pages/Stunting.vue
+++ resources/js/pages/Stunting.vue
@@ -3,7 +3,7 @@ import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
 import PlaceholderPattern from '../components/PlaceholderPattern.vue';
-
+import MapView from '@/components/MapView.vue';
 const breadcrumbs: BreadcrumbItem[] = [
     { title: 'Beranda', href: '/dashboard' },
     { title: 'Stunting', href: '/stunting' },
@@ -13,6 +13,9 @@ const breadcrumbs: BreadcrumbItem[] = [
 <template>
     <Head title="Stunting" />
     <AppLayout :breadcrumbs="breadcrumbs">
+        <div class="relative min-h-[500px] w-full flex-1 rounded-xl border border-sidebar-border/70 dark:border-sidebar-border md:min-h-min">
+                <MapView />
+            </div>
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
                 <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
