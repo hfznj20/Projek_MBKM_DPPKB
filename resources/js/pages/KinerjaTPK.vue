44348d054e6d2a03645f675cb5a3e781f70c38b9
Nurinayabahar2712 <nurinayabahar1227@gmail.com>
2025-05-10 16:59:22 +0700

    BARU NAYA

--- resources/js/pages/KinerjaTPK.vue
+++ resources/js/pages/KinerjaTPK.vue
@@ -12,6 +12,9 @@ const breadcrumbs: BreadcrumbItem[] = [
 
 <template>
     <Head title="Kinerja TPK" />
+    <Head title="Kinerja TPK" />
+    <Head title="Kinerja TPK" />
+    <Head title="Kinerja TPK" />
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
